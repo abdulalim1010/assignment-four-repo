ab4f58afee8111a20852c1087ed77d57c743b2e6
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-17 23:44:24 +0600

    night time

--- cash.js
+++ cash.js
@@ -1,8 +1,14 @@
 /*function signature/sample */
-function cashOut( money ) {
-  const charge = money * (1.75 / 100);
-  return charge;
+function cashOut(money) {
+  
+  if (money < 0 || isNaN(money) ){
+
+return "invalid";
+  }
+  else
+ { const charge = money * (1.75 / 100);
+  return charge;}
 }
 
-const charge = cashOut(6000);
+const charge = cashOut("alin");
 console.log(charge);
